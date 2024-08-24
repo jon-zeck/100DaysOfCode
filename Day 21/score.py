275e2d900984420cf91b682b0e6ca3a85fd3a4da
jon8z <jon8.zeck@gmail.com>
2024-08-24 13:30:10 +0100

    player now wins if score is >= 7 and has at least 2 points over other player

---
+++ Day 21/score.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+from globals import SET_MOVE_FALSE, ALIGNMENT, SCORE_FONT
+
+class Score(Turtle):
+    def __init__(self, location):
+        super().__init__()
+        self.score = 5
+        self.hideturtle()
+        self.penup()
+        self.speed("fastest")
+        self.color("grey")
+        self.goto(location["x"], location["y"])
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"SCORE: {self.score}", SET_MOVE_FALSE, ALIGNMENT, SCORE_FONT)
+
+    def increase_score(self):
+        self.score += 1
+
+    def reset(self):
+        self.score = 0
+        self.update_scoreboard()
+    
